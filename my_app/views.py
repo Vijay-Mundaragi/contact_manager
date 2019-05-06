27a52f0750d38bf5f365df2dd83a590844dce0ea
Vijay-Mundaragi <saivijay010@gmail.com>
2019-05-06 21:48:10 +0530

    Class Based ListView

--- my_app/views.py
+++ my_app/views.py
@@ -1,15 +1,24 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Contact
+from django.views.generic import ListView
 
-
-def home(request):
-    context = {
-        'contacts': Contact.objects.all(),
-    }
-    return render(request, 'index.html', context)
+#===== Function based view ===========
+# def home(request):
+#     context = {
+#         'contacts': Contact.objects.all(),
+#     }
+#     return render(request, 'index.html', context)
 
 def detail(request, id):
     context = {
         'contact': get_object_or_404(Contact, pk=id)
     }
     return render(request, 'detail.html', context)
+
+
+
+#======= Class based View ========
+class HomePageView(ListView):
+    template_name = 'index.html'
+    model = Contact
+    context_object_name = 'contacts'
