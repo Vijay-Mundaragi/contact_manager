5ff41890588f6392faf05f39a6495e808ca1d3b0
Vijay-Mundaragi <saivijay010@gmail.com>
2019-05-08 20:03:52 +0530

    search functionality

--- my_app/urls.py
+++ my_app/urls.py
@@ -2,10 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('', views.home, name="home"),
-
     path('', views.HomePageView.as_view(), name="home"),
-    # path('detail/<int:id>/', views.detail, name="detail")
-
-     path('detail/<int:pk>/', views.ContactDetailView.as_view(), name="detail"),
+    path('detail/<int:pk>/', views.ContactDetailView.as_view(), name="detail"),
+    path('search', views.search, name='search'),
 ]
--- my_app/views.py
+++ my_app/views.py
@@ -1,21 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Contact
 from django.views.generic import ListView, DetailView
-
-#===== Function based view ===========
-# def home(request):
-#     context = {
-#         'contacts': Contact.objects.all(),
-#     }
-#     return render(request, 'index.html', context)
-
-def detail(request, id):
-    context = {
-        'contact': get_object_or_404(Contact, pk=id)
-    }
-    return render(request, 'detail.html', context)
-
-
+from django.db.models import Q
 
 #======= Class based View ========
 class HomePageView(ListView):
@@ -28,3 +14,23 @@ class ContactDetailView(DetailView):
     template_name = 'detail.html'
     model = Contact
     context_object_name = 'contact'
+
+
+#======== Function Based View ======
+def search(request):
+    print(request)
+    if request.GET:
+        search_term = request.GET['search_term']
+        search_results = Contact.objects.filter(
+            Q(name__icontains=search_term) |
+            Q(email__icontains=search_term) |
+            Q(info__icontains=search_term) |
+            Q(phone__iexact=search_term)
+        )
+        context = {
+            'search_term': search_term,
+            'contacts': search_results,
+        }
+        return render(request, 'search.html', context)
+    else:
+        return redirect('home')
